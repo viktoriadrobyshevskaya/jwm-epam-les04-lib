3f9b733a6b0f9736412d73217c65fdc9631e355a
Виктория Дробышевская <75070961+viktoriadrobyshevskaya@users.noreply.github.com>
2022-02-27 20:23:28 +0300

    Add files via upload

---
+++ src/by/epam/jwd/les4/TestLibControl.java
@@ -0,0 +1,8 @@
+package by.epam.jwd.les4;
+
+public class TestLibControl {
+    public static void main(String[] args) {
+        String expression = "5 + 2 * 2 + (5 - 3) * 2";
+        OutputResult.printResult(expression);
+    }
+}
